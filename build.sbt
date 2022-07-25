2f4478c4acc5370fb0128ff1ac5fc43dda365fa9
Alexey Alekhin <alexey.alekhin@tapad.com>
2022-07-25 02:33:20 +0200

    add pom.xml with test deps

--- build.sbt
+++ build.sbt
@@ -1,1 +1,11 @@
-lazy val root = project.in(file("."))
+lazy val root = project
+  .in(file("."))
+  .settings(
+    makePom / artifactPath := baseDirectory.value / "pom.xml",
+    libraryDependencies ++= Seq(
+      "com.github.scopt" %% "scopt" % "4.0.0",
+      "com.github.pathikrit" %% "better-files" % "3.8.0",
+      "eu.timepit" %% "refined" % "0.10.1",
+      "com.google.code.gson" % "gson" % "2.8.8",
+    ),
+  )
--- project/Build.scala
+++ project/Build.scala
@@ -2,26 +2,23 @@ package org.laughedelic.sbt
 
 import sbt._
 import sbt.Keys._
-import sbtdynver.DynVerPlugin.autoImport.dynverSeparator
-import scalafix.sbt.ScalafixPlugin
-import scalafix.sbt.ScalafixPlugin.autoImport._
+// import sbtdynver.DynVerPlugin.autoImport.dynverSeparator
+// import scalafix.sbt.ScalafixPlugin
+// import scalafix.sbt.ScalafixPlugin.autoImport._
 
 object SubProject extends AutoPlugin {
 
   override def trigger = allRequirements
 
-  override def requires = ScalafixPlugin
+  // override def requires = ScalafixPlugin
 
-  override def buildSettings: Seq[Setting[_]] =
-    Seq(
-      organization := "laughedelic",
-      dynverSeparator := "-",
-      semanticdbEnabled := true, // enable SemanticDB
-      semanticdbVersion := scalafixSemanticdb.revision, // use Scalafix compatible version
-      scalafixDependencies ++= Seq(
-        "com.github.vovapolu" %% "scaluzzi" % "0.1.17",
-        "com.eed3si9n.fix" %% "scalafix-noinfer" % "0.1.0-M1",
-      ),
-    )
+  val repo = "https://github.com/laughedelic/test-pom-export"
+
+  override def buildSettings: Seq[Setting[_]] = Seq(
+    organization := "laughedelic",
+    publishMavenStyle := true,
+    scmInfo := Option(ScmInfo(url(repo), s"$repo.git")),
+    homepage := Option(url(repo)),
+  )
 
 }
